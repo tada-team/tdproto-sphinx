86ee10ea6a49ae521a323d558827731b33d92b81
igo95862 <igo95862@yandex.ru>
2021-04-23 16:07:57 +0300

    Use RTD theme only if its installed

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,16 @@
-project='Tdproto docs'
+project = 'Tdproto docs'
 locale_dirs = ['locale/']
-gettext_uuid=True
+gettext_uuid = True
 
-extensions = [
-    "sphinx_rtd_theme",
-]
+try:
+    import sphinx_rtd_theme
+    global extensions
+    extensions = [
+        "sphinx_rtd_theme",
+    ]
+    del sphinx_rtd_theme
+except ImportError:
+    ...
+
 
 html_theme = "sphinx_rtd_theme"
